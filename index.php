ab6fc409c36ace268bb2a30c139fbff6e21833f0
J.J <99402892+JuliaC261@users.noreply.github.com>
2022-03-15 09:19:40 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+
+ <form action="soma.php" method="post">
+   Número 1: <input type="text" name="n1"><br/>
+   Número 2: <input type="text" name="n2"><br/>
+   <input type="submit" value="calcular"> <br/>
+Resultado: <input type="text" width="5"><br/>
+ </form>
+  
+  </body>
+</html>
